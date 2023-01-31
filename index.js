3ee8d27cd819e8a9f21f6a9d4c0946747420d0d8
Simon <yash1441@yahoo.com>
2023-01-31 13:13:16 +0530

    Update index.js
    Stringify debug

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ client.on("ready", () => {
 	const guild = client.guilds.cache.get(process.env.EVO_SERVER);
 	let test = guild.members;
 	console.log({ guild });
-	logger.debug(JSON.parse(test));
+	logger.debug(JSON.stringiy(test));
 
 	// cron.schedule(
 	// 	"0 */30 * * * *",
