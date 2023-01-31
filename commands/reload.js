137cc3e4a984de1c0a3d00fef000d2958b800398
Simon <yash1441@yahoo.com>
2023-01-31 12:22:42 +0530

    Update reload.js
    Changed debug

--- commands/reload.js
+++ commands/reload.js
@@ -495,7 +495,7 @@ module.exports = {
 			let records = [];
 			const guild = client.guilds.cache.get(process.env.EVO_SERVER);
 			let test = await guild.members.cache.get("481584916853948416");
-			logger.debug(response.data.items[0].fields["Discord ID"]);
+			logger.debug(test);
 
 			for (const record of response.data.items) {
 				const member = guild.members.cache.get(record.fields["Discord ID"]);
