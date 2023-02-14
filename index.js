11a4e65fa0840b6826de85efca47a50ca2d70a2b
Simon <yash1441@yahoo.com>
2023-02-14 16:18:37 +0530

    Commented debugs

--- index.js
+++ index.js
@@ -2904,22 +2904,18 @@ function onlyDigits(string) {
 }
 
 async function checkMemberRole(client, guildId, userId, roleId) {
-	logger.debug(
-		`Checking if user ${userId} has role ${roleId} in guild ${guildId}`
-	);
+	// logger.debug(`Checking if user ${userId} has role ${roleId} in guild ${guildId}`);
 	const guild = client.guilds.cache.get(guildId);
 	const member = await guild.members.fetch(userId);
 	if (member == undefined) {
-		logger.debug(`User ${userId} not found in guild ${guildId}`);
+		// logger.debug(`User ${userId} not found in guild ${guildId}`);
 		return false;
 	}
 	if (member.roles.cache.has(roleId)) {
-		logger.debug(`User ${userId} has role ${roleId} in guild ${guildId}`);
+		// logger.debug(`User ${userId} has role ${roleId} in guild ${guildId}`);
 		return true;
 	} else {
-		logger.debug(
-			`User ${userId} does not have role ${roleId} in guild ${guildId}`
-		);
+		// logger.debug(`User ${userId} does not have role ${roleId} in guild ${guildId}`);
 		return false;
 	}
 }
