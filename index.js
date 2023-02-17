92a8e39dcc8e50328e69df916a13b64446ac038b
Simon <yash1441@yahoo.com>
2023-02-17 12:59:54 +0530

    Added more platforms to creator form
    twitter, twitch, instagram

--- index.js
+++ index.js
@@ -2891,7 +2891,7 @@ function checkURL(text) {
 		text = text.replace("www.", "");
 	}
 	const expression =
-		/^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.be|tiktok.com|taptap.io)\/.+$/;
+		/^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.be|tiktok.com|taptap.io|twitter.com|instagram.com|twitch.com)\/.+$/;
 	const regex = new RegExp(expression);
 
 	if (text.match(regex)) {
