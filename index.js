3e95f9dd4b9e62f887d55363cd3b07d19fc3f38f
Simon <yash1441@yahoo.com>
2023-03-06 12:24:27 +0530

    Changed followUp to send

--- index.js
+++ index.js
@@ -2010,9 +2010,8 @@ client.on("interactionCreate", async (interaction) => {
 			const selection = interaction.values[0];
 			const category = interaction.customId.substring(7);
 
-			await interaction.update({
+			await interaction.channel.send({
 				content: `**${category}**\n${selection}\n\nPlease upload **one** screenshot for the bug in the next **60** seconds below.`,
-				components: [],
 			});
 
 			const filter = (m) =>
