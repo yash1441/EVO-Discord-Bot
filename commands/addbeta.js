1cec4c7b1de7b42b7ad4b7440d0a47538edef4c0
Simon <yash1441@yahoo.com>
2023-03-07 20:45:28 +0530

    Removed check

--- commands/addbeta.js
+++ commands/addbeta.js
@@ -17,25 +17,19 @@ module.exports = {
 		await interaction.deferReply({ ephemeral: true });
 		const target = await interaction.options.getUser("user");
 
-		if (target.roles.cache.has(process.env.BETA_ROLE)) {
-			await interaction.editReply({
-				content: `${target} already has beta tester role.`,
-			});
-		} else {
-			await target.roles.add(process.env.BETA_ROLE);
-			await interaction.editReply({
-				content: `Added beta tester role to ${target}.`,
-			});
+		await target.roles.add(process.env.BETA_ROLE);
+		await interaction.editReply({
+			content: `Added beta tester role to ${target}.`,
+		});
 
-			const embed = new EmbedBuilder()
-				.setTitle("Beta Tester Role Added")
-				.setDescription(
-					`${interaction.user} added beta tester role to ${target}`
-				);
+		const embed = new EmbedBuilder()
+			.setTitle("Beta Tester Role Added")
+			.setDescription(
+				`${interaction.user} added beta tester role to ${target}`
+			);
 
-			client.channels
-				.fetch("951854256477077554")
-				.then((channel) => channel.send({ embeds: [embed] }));
-		}
+		client.channels
+			.fetch("951854256477077554")
+			.then((channel) => channel.send({ embeds: [embed] }));
 	},
 };
