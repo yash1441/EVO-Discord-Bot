41188203ec9e8a5f84bedaa3c5d7efdacaaf11de
Simon <yash1441@yahoo.com>
2023-04-04 16:30:14 +0530

    Removed stop

--- index.js
+++ index.js
@@ -2567,7 +2567,6 @@ client.on("messageCreate", async (message) => {
 			prompt: finalPrompt,
 			temperature: 0.9,
 			max_tokens: 100,
-			stop: [`${message.author.username}:`, "AI:"],
 		});
 
 		console.log(gptResponse.data.choices);
