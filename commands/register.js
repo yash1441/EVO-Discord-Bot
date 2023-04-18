c1e58b99d2b44f08c975c02874d8eca8373399eb
Simon <yash1441@yahoo.com>
2023-04-18 16:55:44 +0530

    Added register and update

---
+++ commands/register.js
@@ -0,0 +1,90 @@
+const {
+	SlashCommandBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChannelType,
+	StringSelectMenuBuilder,
+	EmbedBuilder,
+} = require("discord.js");
+const feishu = require("../feishu.js");
+const logger = require("../logging/logger.js");
+require("dotenv").config();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("register")
+		.setDescription("Register for Community Series.")
+		.setDMPermission(false)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("team")
+				.setDescription("Register a team as leader.")
+				.addStringOption((option) =>
+					option
+						.setName("name")
+						.setDescription("Enter the name for your team.")
+						.setRequired(true)
+						.setMaxLength(30)
+				)
+				.addIntegerOption((option) =>
+					option
+						.setName("role-id")
+						.setDescription("Enter your in-game Role ID.")
+						.setRequired(true)
+						.setMinValue(0)
+				)
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("member")
+				.setDescription("Register a member for your team.")
+				.addUserOption((option) =>
+					option
+						.setName("user")
+						.setDescription("Select your team member.")
+						.setRequired(true)
+				)
+				.addIntegerOption((option) =>
+					option
+						.setName("role-id")
+						.setDescription("Enter your team member's in-game Role ID.")
+						.setRequired(true)
+						.setMinValue(0)
+				)
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("status")
+				.setDescription("Check your team's registration status.")
+		),
+
+	async execute(interaction) {
+		const subCommand = interaction.options.getSubcommand();
+
+		if (subCommand === "team") {
+			const teamLeader = interaction.user;
+			const teamName = interaction.options.getString("name");
+			const teamLeaderRoleId = interaction.options.getInteger("role-id");
+
+			await interaction.reply({
+				content: `Registering team **${teamName}** with the leader as **${teamLeader.user.tag}** *(Role ID: ${teamLeaderRoleId})*...`,
+				ephemeral: true,
+			});
+		} else if (subCommand === "member") {
+			const teamLeader = interaction.user;
+			const teamMember = interaction.options.getUser("user");
+			const teamMemberRoleId = interaction.options.getInteger("role-id");
+
+			await interaction.reply({
+				content: `Registering team member **${teamMember.user.tag}** *(Role ID: ${teamMemberRoleId})* for team leader **${teamLeader.user.tag}**...`,
+				ephemeral: true,
+			});
+		} else if (subCommand === "status") {
+			await interaction.reply({
+				content: "Checking registration status...",
+				ephemeral: true,
+			});
+		}
+	},
+};
---
+++ commands/update.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder, ChannelType } = require("discord.js");
+const logger = require("../logging/logger.js");
+require("dotenv").config();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("update")
+		.setDescription("Update a command.")
+		.addStringOption((option) =>
+			option
+				.setName("command")
+				.setDescription("The command to reload.")
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		if (interaction.user.id != process.env.MY_ID) {
+			return;
+		}
+
+		const commandName = interaction.options
+			.getString("command", true)
+			.toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+
+		if (!command) {
+			return interaction.reply(
+				`There is no command with name \`${commandName}\`!`
+			);
+		}
+
+		delete require.cache[require.resolve(`./${command.data.name}.js`)];
+
+		try {
+			interaction.client.commands.delete(command.data.name);
+			const newCommand = require(`./${command.data.name}.js`);
+			interaction.client.commands.set(newCommand.data.name, newCommand);
+			await interaction.reply(
+				`Command \`${newCommand.data.name}\` was reloaded!`
+			);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply(
+				`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``
+			);
+		}
+	},
+};
